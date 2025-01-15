bb2fbca5e955d1477743ce58745e755ee4b8cb7c
M.B.J. Donners <m.donners@student.fontys.nl>
2025-01-15 02:02:23 +0100

    fix CORS typo

--- AshamedApp.API/Program.cs
+++ AshamedApp.API/Program.cs
@@ -52,7 +52,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         builder =>
         {
-            builder.WithOrigins("http://localhost:4173,","http://localhost:5173")
+            builder.WithOrigins("http://localhost:4173","http://localhost:5173")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
